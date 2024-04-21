20e21f03c726617e48f5f12eb70eb5c793e7bedd
vgroux <victorgroux99@gmail.com>
2024-04-21 07:32:31 +0100

    ajout flag ending (degeu pas transparent) + loadScene fin avec compteur mort et timer (timer not ok)

---
+++ MyGrowingCompany/Assets/vgroux/script/sc_EndingScene_Manager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sc_EndingScene_Manager : MonoBehaviour
+{
+	public Text deathCountText;
+	public Text timer;
+
+	private void Start()
+	{
+		deathCountText.text = "Player died: " + sc_GameSession_Manager.instance.playerDeathCount + " times.";
+		timer.text = "Your final time is: " + (Time.time - sc_GameSession_Manager.instance.startTime) + " seconds.";
+	}
+}
